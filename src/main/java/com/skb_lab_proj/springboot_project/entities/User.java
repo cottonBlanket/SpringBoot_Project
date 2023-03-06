fd85f18e52b910fbc8ed0f0f97a58dbfc1133fdc
Danil <donul.mukhametshin.03@gmail.com>
2023-03-07 02:33:41 +0500

    Test architecture

---
+++ src/main/java/com/skb_lab_proj/springboot_project/entities/User.java
@@ -0,0 +1,64 @@
+package com.skb_lab_proj.springboot_project.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "USERS")
+public class User implements Serializable {
+    @Id
+    private Long id;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Password")
+    private String password;
+
+    public User() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
