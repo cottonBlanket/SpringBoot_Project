163477dc7479ba83454e4c7db359100f370c822d
Danil <donul.mukhametshin.03@gmail.com>
2023-03-07 02:01:09 +0500

    it's OK

---
+++ src/main/java/com/skb_lab_proj/springboot_project/SpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skb_lab_proj.springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skb_lab_proj/springboot_project/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skb_lab_proj.springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
